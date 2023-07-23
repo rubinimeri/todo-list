1fb78083ac3b1f04127ec7887eaa7cbdb4c61378
Rubin <rubinimeri6@gmail.com>
2023-07-23 15:38:30 +0200

    Add html elements and style

---
+++ src/create-todos.js
@@ -0,0 +1,6 @@
+// Create todoItem factory function
+
+export default function todoItem(title, description, dueDate, priority) {
+    return { title, description, dueDate, priority };
+}
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-document.write("Hello");
+import './style.css';
+import todoItem from "./create-todos";
+
+const item = todoItem("Study", "study session for about 2 hours", "7/23/2023", "high");
+console.log(item);
